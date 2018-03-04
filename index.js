e5c5ebf2e2ca3636c61ff82b111c9e1caef2e4ea
SyraD <Syradietrich@gmail.com>
2018-03-04 12:23:23 -0500

    tests

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/login', function(request, response) {
 app.post('/login', function(request, response) {
 console.log(request.body);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM users WHERE username = teacher AND password = dogv0mit LIMIT 1', function(err, result) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
@@ -47,7 +47,7 @@ var pg = require('pg');
 app.get('/db', function (request, response) {
 var temp = '\' OR TRUE'
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM users WHERE username = $1::text AND password = $2::text LIMIT 1', ['teacher', temp], function(err, result) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
