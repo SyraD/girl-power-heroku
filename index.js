8638871dc5e299025ae3adbeca31d7e9afa4b66b
SyraD <Syradietrich@gmail.com>
2018-03-04 12:06:28 -0500

    static string sql query

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ app.get('/login', function(request, response) {
 
 app.post('/login', function(request, response) {
 console.log(request.body);
-console.log("Sarah test version");
+var query =
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM users WHERE username = $1::text AND password = $2::text LIMIT 1', [request.body.user, request.body.password], function(err, result) {
+    client.query('SELECT * FROM users WHERE username = teacher AND password = dogv0mit LIMIT 1', [request.body.user, request.body.password], function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
