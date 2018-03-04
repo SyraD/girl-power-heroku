527920371f077789a055376099de8fabef4ca7e1
Zaza Soriano <zaza.soriano@jhuapl.edu>
2018-03-03 21:51:36 -0500

    work in progress

--- index.js
+++ index.js
@@ -15,20 +15,12 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/login', function(request, response) {
+  response.render('pages/login');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
 });
 
 var pg = require('pg');
