bdb2788a15bbbca3df8d0e063354fc7165bb49b8
Zaza Soriano <zaza.soriano@jhuapl.edu>
2018-03-03 21:03:59 -0500

    stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-var $ = require('jQuery');
+//var $ = require('jQuery');
 
 app.set('port', (process.env.PORT || 5000));
 
