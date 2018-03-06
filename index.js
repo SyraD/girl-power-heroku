d9e106b1289748b7c6db7620fdbda91733df732f
Zaza Soriano <zaza.soriano@jhuapl.edu>
2018-03-06 13:56:05 -0500

    tabulator test

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const middlewares = [
 var app = express();
 var $ = require('jquery');
 
+require('jquery.tabulator');
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
