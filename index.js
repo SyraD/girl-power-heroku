473ec6deee73152b1ef01a90b981a1ec1477e193
SyraD <Syradietrich@gmail.com>
2018-03-06 21:31:00 -0500

    zaza merge

--- index.js
+++ index.js
@@ -21,12 +21,11 @@ app.get('/', function(request, response) {
 });
 
 app.get('/login', function(request, response) {
-  response.render('pages/login');
+  response.render('pages/login', {err: false});
 });
 
 app.post('/login', function(request, response) {
-console.log(request.body.user);
-console.log(request.body.password)
+console.log(request.body);
 var query =  "SELECT * FROM users WHERE username = '" + request.body.user + "' AND password = '" + request.body.password + "'";
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query(query, function(err, result) {
@@ -34,15 +33,31 @@ var query =  "SELECT * FROM users WHERE username = '" + request.body.user + "' A
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { console.log(result.rows.length);
+            if(result.rows.length == 1){
+                response.render('pages/teacher', {results: result.rows} );
+            }
+            else{
+                console.log("invalid password");
+                response.render('pages/login', {err: query} );
+            }
+       }
     });
   });
 
 });
+
+app.post('/update', function(request, response) {
+  console.log(request.body);
+});
+
+app.post('/add', function(request, response) {
+  console.log(request.body);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 var pg = require('pg');
-
